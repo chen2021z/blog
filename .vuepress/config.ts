e1ab91746aceca0c6739e1d4bfb60aa6db0acd1a
Chen <2934570984@qq.com>
2023-08-22 23:06:17 +0800

    feat: 制定base仓库

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -5,6 +5,7 @@ import recoTheme from "vuepress-theme-reco";
 export default defineUserConfig({
   title: "vuepress-theme-reco",
   description: "Just playing around",
+  base:'/blog/',
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
     logo: "/logo.png",
